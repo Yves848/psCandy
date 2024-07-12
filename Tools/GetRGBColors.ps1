fd16986e112cdfe0af65625dbbf5b7c63f998ef4
yves848 <yves.godart@gmail.com>
2024-07-12 12:40:24 +0200

    WIP

---
+++ Tools/GetRGBColors.ps1
@@ -0,0 +1,18 @@
+using module psCandy
+. .\themes.ps1
+
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8 
+[console]::Clear()
+
+[system.Drawing.color] | Get-Member -Static -MemberType Properties | ForEach-Object {
+  
+  $color = [System.Drawing.Color]::"$($_.Name)"
+  $template = @"
+  $($color.Name)= @{
+    R = $($color.R)
+    G = $($color.G)
+    B = $($color.B)
+  }
+"@
+  $template
+} | Out-File .\test.ps1
---
+++ Tools/test.ps1
@@ -0,0 +1,718 @@
+$Colors=@{
+  AliceBlue= @{
+    R = 240
+    G = 248
+    B = 255
+  }
+  AntiqueWhite= @{
+    R = 250
+    G = 235
+    B = 215
+  }
+  Aqua= @{
+    R = 0
+    G = 255
+    B = 255
+  }
+  Aquamarine= @{
+    R = 127
+    G = 255
+    B = 212
+  }
+  Azure= @{
+    R = 240
+    G = 255
+    B = 255
+  }
+  Beige= @{
+    R = 245
+    G = 245
+    B = 220
+  }
+  Bisque= @{
+    R = 255
+    G = 228
+    B = 196
+  }
+  Black= @{
+    R = 0
+    G = 0
+    B = 0
+  }
+  BlanchedAlmond= @{
+    R = 255
+    G = 235
+    B = 205
+  }
+  Blue= @{
+    R = 0
+    G = 0
+    B = 255
+  }
+  BlueViolet= @{
+    R = 138
+    G = 43
+    B = 226
+  }
+  Brown= @{
+    R = 165
+    G = 42
+    B = 42
+  }
+  BurlyWood= @{
+    R = 222
+    G = 184
+    B = 135
+  }
+  CadetBlue= @{
+    R = 95
+    G = 158
+    B = 160
+  }
+  Chartreuse= @{
+    R = 127
+    G = 255
+    B = 0
+  }
+  Chocolate= @{
+    R = 210
+    G = 105
+    B = 30
+  }
+  Coral= @{
+    R = 255
+    G = 127
+    B = 80
+  }
+  CornflowerBlue= @{
+    R = 100
+    G = 149
+    B = 237
+  }
+  Cornsilk= @{
+    R = 255
+    G = 248
+    B = 220
+  }
+  Crimson= @{
+    R = 220
+    G = 20
+    B = 60
+  }
+  Cyan= @{
+    R = 0
+    G = 255
+    B = 255
+  }
+  DarkBlue= @{
+    R = 0
+    G = 0
+    B = 139
+  }
+  DarkCyan= @{
+    R = 0
+    G = 139
+    B = 139
+  }
+  DarkGoldenrod= @{
+    R = 184
+    G = 134
+    B = 11
+  }
+  DarkGray= @{
+    R = 169
+    G = 169
+    B = 169
+  }
+  DarkGreen= @{
+    R = 0
+    G = 100
+    B = 0
+  }
+  DarkKhaki= @{
+    R = 189
+    G = 183
+    B = 107
+  }
+  DarkMagenta= @{
+    R = 139
+    G = 0
+    B = 139
+  }
+  DarkOliveGreen= @{
+    R = 85
+    G = 107
+    B = 47
+  }
+  DarkOrange= @{
+    R = 255
+    G = 140
+    B = 0
+  }
+  DarkOrchid= @{
+    R = 153
+    G = 50
+    B = 204
+  }
+  DarkRed= @{
+    R = 139
+    G = 0
+    B = 0
+  }
+  DarkSalmon= @{
+    R = 233
+    G = 150
+    B = 122
+  }
+  DarkSeaGreen= @{
+    R = 143
+    G = 188
+    B = 143
+  }
+  DarkSlateBlue= @{
+    R = 72
+    G = 61
+    B = 139
+  }
+  DarkSlateGray= @{
+    R = 47
+    G = 79
+    B = 79
+  }
+  DarkTurquoise= @{
+    R = 0
+    G = 206
+    B = 209
+  }
+  DarkViolet= @{
+    R = 148
+    G = 0
+    B = 211
+  }
+  DeepPink= @{
+    R = 255
+    G = 20
+    B = 147
+  }
+  DeepSkyBlue= @{
+    R = 0
+    G = 191
+    B = 255
+  }
+  DimGray= @{
+    R = 105
+    G = 105
+    B = 105
+  }
+  DodgerBlue= @{
+    R = 30
+    G = 144
+    B = 255
+  }
+  Empty= @{
+    R = 0
+    G = 0
+    B = 0
+  }
+  Firebrick= @{
+    R = 178
+    G = 34
+    B = 34
+  }
+  FloralWhite= @{
+    R = 255
+    G = 250
+    B = 240
+  }
+  ForestGreen= @{
+    R = 34
+    G = 139
+    B = 34
+  }
+  Fuchsia= @{
+    R = 255
+    G = 0
+    B = 255
+  }
+  Gainsboro= @{
+    R = 220
+    G = 220
+    B = 220
+  }
+  GhostWhite= @{
+    R = 248
+    G = 248
+    B = 255
+  }
+  Gold= @{
+    R = 255
+    G = 215
+    B = 0
+  }
+  Goldenrod= @{
+    R = 218
+    G = 165
+    B = 32
+  }
+  Gray= @{
+    R = 128
+    G = 128
+    B = 128
+  }
+  Green= @{
+    R = 0
+    G = 128
+    B = 0
+  }
+  GreenYellow= @{
+    R = 173
+    G = 255
+    B = 47
+  }
+  Honeydew= @{
+    R = 240
+    G = 255
+    B = 240
+  }
+  HotPink= @{
+    R = 255
+    G = 105
+    B = 180
+  }
+  IndianRed= @{
+    R = 205
+    G = 92
+    B = 92
+  }
+  Indigo= @{
+    R = 75
+    G = 0
+    B = 130
+  }
+  Ivory= @{
+    R = 255
+    G = 255
+    B = 240
+  }
+  Khaki= @{
+    R = 240
+    G = 230
+    B = 140
+  }
+  Lavender= @{
+    R = 230
+    G = 230
+    B = 250
+  }
+  LavenderBlush= @{
+    R = 255
+    G = 240
+    B = 245
+  }
+  LawnGreen= @{
+    R = 124
+    G = 252
+    B = 0
+  }
+  LemonChiffon= @{
+    R = 255
+    G = 250
+    B = 205
+  }
+  LightBlue= @{
+    R = 173
+    G = 216
+    B = 230
+  }
+  LightCoral= @{
+    R = 240
+    G = 128
+    B = 128
+  }
+  LightCyan= @{
+    R = 224
+    G = 255
+    B = 255
+  }
+  LightGoldenrodYellow= @{
+    R = 250
+    G = 250
+    B = 210
+  }
+  LightGray= @{
+    R = 211
+    G = 211
+    B = 211
+  }
+  LightGreen= @{
+    R = 144
+    G = 238
+    B = 144
+  }
+  LightPink= @{
+    R = 255
+    G = 182
+    B = 193
+  }
+  LightSalmon= @{
+    R = 255
+    G = 160
+    B = 122
+  }
+  LightSeaGreen= @{
+    R = 32
+    G = 178
+    B = 170
+  }
+  LightSkyBlue= @{
+    R = 135
+    G = 206
+    B = 250
+  }
+  LightSlateGray= @{
+    R = 119
+    G = 136
+    B = 153
+  }
+  LightSteelBlue= @{
+    R = 176
+    G = 196
+    B = 222
+  }
+  LightYellow= @{
+    R = 255
+    G = 255
+    B = 224
+  }
+  Lime= @{
+    R = 0
+    G = 255
+    B = 0
+  }
+  LimeGreen= @{
+    R = 50
+    G = 205
+    B = 50
+  }
+  Linen= @{
+    R = 250
+    G = 240
+    B = 230
+  }
+  Magenta= @{
+    R = 255
+    G = 0
+    B = 255
+  }
+  Maroon= @{
+    R = 128
+    G = 0
+    B = 0
+  }
+  MediumAquamarine= @{
+    R = 102
+    G = 205
+    B = 170
+  }
+  MediumBlue= @{
+    R = 0
+    G = 0
+    B = 205
+  }
+  MediumOrchid= @{
+    R = 186
+    G = 85
+    B = 211
+  }
+  MediumPurple= @{
+    R = 147
+    G = 112
+    B = 219
+  }
+  MediumSeaGreen= @{
+    R = 60
+    G = 179
+    B = 113
+  }
+  MediumSlateBlue= @{
+    R = 123
+    G = 104
+    B = 238
+  }
+  MediumSpringGreen= @{
+    R = 0
+    G = 250
+    B = 154
+  }
+  MediumTurquoise= @{
+    R = 72
+    G = 209
+    B = 204
+  }
+  MediumVioletRed= @{
+    R = 199
+    G = 21
+    B = 133
+  }
+  MidnightBlue= @{
+    R = 25
+    G = 25
+    B = 112
+  }
+  MintCream= @{
+    R = 245
+    G = 255
+    B = 250
+  }
+  MistyRose= @{
+    R = 255
+    G = 228
+    B = 225
+  }
+  Moccasin= @{
+    R = 255
+    G = 228
+    B = 181
+  }
+  NavajoWhite= @{
+    R = 255
+    G = 222
+    B = 173
+  }
+  Navy= @{
+    R = 0
+    G = 0
+    B = 128
+  }
+  OldLace= @{
+    R = 253
+    G = 245
+    B = 230
+  }
+  Olive= @{
+    R = 128
+    G = 128
+    B = 0
+  }
+  OliveDrab= @{
+    R = 107
+    G = 142
+    B = 35
+  }
+  Orange= @{
+    R = 255
+    G = 165
+    B = 0
+  }
+  OrangeRed= @{
+    R = 255
+    G = 69
+    B = 0
+  }
+  Orchid= @{
+    R = 218
+    G = 112
+    B = 214
+  }
+  PaleGoldenrod= @{
+    R = 238
+    G = 232
+    B = 170
+  }
+  PaleGreen= @{
+    R = 152
+    G = 251
+    B = 152
+  }
+  PaleTurquoise= @{
+    R = 175
+    G = 238
+    B = 238
+  }
+  PaleVioletRed= @{
+    R = 219
+    G = 112
+    B = 147
+  }
+  PapayaWhip= @{
+    R = 255
+    G = 239
+    B = 213
+  }
+  PeachPuff= @{
+    R = 255
+    G = 218
+    B = 185
+  }
+  Peru= @{
+    R = 205
+    G = 133
+    B = 63
+  }
+  Pink= @{
+    R = 255
+    G = 192
+    B = 203
+  }
+  Plum= @{
+    R = 221
+    G = 160
+    B = 221
+  }
+  PowderBlue= @{
+    R = 176
+    G = 224
+    B = 230
+  }
+  Purple= @{
+    R = 128
+    G = 0
+    B = 128
+  }
+  RebeccaPurple= @{
+    R = 102
+    G = 51
+    B = 153
+  }
+  Red= @{
+    R = 255
+    G = 0
+    B = 0
+  }
+  RosyBrown= @{
+    R = 188
+    G = 143
+    B = 143
+  }
+  RoyalBlue= @{
+    R = 65
+    G = 105
+    B = 225
+  }
+  SaddleBrown= @{
+    R = 139
+    G = 69
+    B = 19
+  }
+  Salmon= @{
+    R = 250
+    G = 128
+    B = 114
+  }
+  SandyBrown= @{
+    R = 244
+    G = 164
+    B = 96
+  }
+  SeaGreen= @{
+    R = 46
+    G = 139
+    B = 87
+  }
+  SeaShell= @{
+    R = 255
+    G = 245
+    B = 238
+  }
+  Sienna= @{
+    R = 160
+    G = 82
+    B = 45
+  }
+  Silver= @{
+    R = 192
+    G = 192
+    B = 192
+  }
+  SkyBlue= @{
+    R = 135
+    G = 206
+    B = 235
+  }
+  SlateBlue= @{
+    R = 106
+    G = 90
+    B = 205
+  }
+  SlateGray= @{
+    R = 112
+    G = 128
+    B = 144
+  }
+  Snow= @{
+    R = 255
+    G = 250
+    B = 250
+  }
+  SpringGreen= @{
+    R = 0
+    G = 255
+    B = 127
+  }
+  SteelBlue= @{
+    R = 70
+    G = 130
+    B = 180
+  }
+  Tan= @{
+    R = 210
+    G = 180
+    B = 140
+  }
+  Teal= @{
+    R = 0
+    G = 128
+    B = 128
+  }
+  Thistle= @{
+    R = 216
+    G = 191
+    B = 216
+  }
+  Tomato= @{
+    R = 255
+    G = 99
+    B = 71
+  }
+  Transparent= @{
+    R = 255
+    G = 255
+    B = 255
+  }
+  Turquoise= @{
+    R = 64
+    G = 224
+    B = 208
+  }
+  Violet= @{
+    R = 238
+    G = 130
+    B = 238
+  }
+  Wheat= @{
+    R = 245
+    G = 222
+    B = 179
+  }
+  White= @{
+    R = 255
+    G = 255
+    B = 255
+  }
+  WhiteSmoke= @{
+    R = 245
+    G = 245
+    B = 245
+  }
+  Yellow= @{
+    R = 255
+    G = 255
+    B = 0
+  }
+  YellowGreen= @{
+    R = 154
+    G = 205
+    B = 50
+  }
+}
+
