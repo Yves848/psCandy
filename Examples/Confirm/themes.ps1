9bd4815026b7949ac0de2d69001e48ca9866d7e1
yves848 <yves.godart@gmail.com>
2024-07-12 15:55:28 +0200

    WIP

---
+++ Examples/Confirm/themes.ps1
@@ -0,0 +1,24 @@
+﻿using module psCandy
+
+$Theme = @{
+  "list"= @{
+    "SearchColor" = [Colors]::BlueViolet()
+    "SelectedColor" = [Colors]::Yellow()
+    "SelectedStyle" = [Styles]::Underline
+    "FilterColor" = [Colors]::Orange()
+    "NoFilterColor" = [Colors]::Orange()
+    "FilterStyle" = [Styles]::Underline
+    "Checked"="◉"
+    "Unchecked"="○"
+  }
+  "spinner" = @{
+    "spincolor"= [Colors]::MediumOrchid()
+    "spinType"= "Dots"
+  }
+  "choice" = @{
+    "SelectedForeground" = [Colors]::BlueViolet()
+    "SelectedBackground" = [Colors]::White()
+    "OptionColor" = [Colors]::SkyBlue()
+    "MessageColor" = [Colors]::Coral()
+  }
+}
