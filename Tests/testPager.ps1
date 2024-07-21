7bfb5643ef27189ef63d924f3303fd7fbe7a7b90
yves848 <yves.godart@gmail.com>
2024-07-21 14:33:24 +0200

    WIP

--- Tests/testPager.ps1
+++ Tests/testPager.ps1
@@ -5,7 +5,19 @@ param (
 )
 
 $Pager = [Pager]::New($path)
-$Pager.actions.add(33, { $Pager.index = $Pager.index - 8} )
-$Pager.actions.add(34, { $Pager.index = $Pager.index + 8 })
+$Pager.actions.add(33, { 
+  $Pager.index = $Pager.index - 8
+  if ($Pager.index -lt 0) {
+    $Pager.index = 0
+  }
+} )
+$Pager.actions.add(34, { 
+  $oldIndex = $Pager.index
+  $Pager.index = $Pager.index + 8
+  if ($Pager.index -ge ($Pager.height -1)) {
+    $pager.offset = ($pager.offset + $pager.height)
+    $Pager.index = $Pager.height - $oldIndex
+  }
+})
 
 $Pager.Display()
